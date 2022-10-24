dcceaa73429a16874ad8c82633cf8dc1ba2701ee
amir_g <amir_g@holdingbp.ru>
2022-10-25 00:08:36 +0300

    Добавил описание на страницу "О себе"

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import classes from './AboutMe.module.css'
 
-import Paragraph from '../../components/UI/texts/Paragraph'
 import HeaderText from '../../components/UI/texts/HeaderText'
+import Paragraph from '../../components/UI/texts/Paragraph'
+import ImportantText from '../../components/UI/texts/ImportantText'
 import Anchor from '../../components/UI/texts/Anchor'
 
 function AboutMe() {
@@ -30,19 +31,20 @@ function AboutMe() {
             <Paragraph>
                 На первую работу в IT, я устроился 16 ноября 2021 года, в компанию "AVTOBUS1", 
                 на должность "Frontent-разработчик". На данный момент работаю там же. <br/>
-                Наш стек технологий Backend: C#, .NET <br/>
-                Наш стек технологий Frontend: HTML, CSS, JavaScript, JQuery, Kendo UI MVC <br/>
+                Стек технологий Backend: <br/><ImportantText>C#, .NET</ImportantText> <br/>
+                Стек технологий Frontend: <br/><ImportantText>HTML, CSS, JavaScript, JQuery, Kendo UI MVC</ImportantText> <br/>
                 Список проектов над которыми я работал в компании: <br/>
                 1) <Anchor href="https://erp.holdingbp.ru/">Корпоративная ERP-система</Anchor> <br/>
                 2) <Anchor href="https://producercrm.ru/">Собственная CRM-система</Anchor> <br/>
                 3) <Anchor href="http://cms.holdingbp.ru/">CMS-система</Anchor> <br/>
                 4) Сайт <Anchor href="https://avtobus1.ru/">Avtobus1</Anchor> <br/>
-                5) Сайт <Anchor href="https://avtobustrans.ru/">Avtobustrans</Anchor> <br/>
-                6) Сайт <Anchor href="#">ZakBus</Anchor> <br/>
-                7) Сайт <Anchor href="http://edembus.ru/">EdemBus</Anchor> <br/>
-                8) Сайт <Anchor href="#">Charterbus</Anchor> <br/>
-                9) Сайт <Anchor href="#">Bus1</Anchor> <br/>
-                10) Сайт <Anchor href="https://nazmiev.club/">Nazmiev.club</Anchor>
+                6) Сайт <Anchor href="http://www.avtobus05.ru/">Avtobus05</Anchor> <br/>
+                7) Сайт <Anchor href="http://baikal-bus.ru/">Baikal-bus</Anchor> <br/>
+                8) Сайт <Anchor href="http://xn--e1aidijbigept.xn--p1ai/">Рейскомфорт</Anchor> <br/>
+                8) Сайт <Anchor href="https://dostavkasotrudnikov.ru/">Dostavkasotrudnikov</Anchor> <br/>
+                9) Сайт <Anchor href="https://avtobustrans.ru/">Avtobustrans</Anchor> <br/>
+                10) Сайт <Anchor href="http://edembus.ru/">EdemBus</Anchor> <br/>
+                11) Сайт <Anchor href="https://nazmiev.club/">Nazmiev.club</Anchor>
             </Paragraph>
         </div>
     )
